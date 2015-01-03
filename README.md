9aac0d1db4d2b633765e22b638674c45ef5ebd53
calexity <alexa@carbonfive.com>
2015-01-03 14:27:21 -0800

    add tabulate app

---
+++ README.md
@@ -0,0 +1,37 @@
+## Tabulate
+
+This is a simple tip calculator iOS app made as part of the prework for the [https://courses.codepath.com/snippets/ios_for_designers/thanks_for_applying](CodePath iOS for designers class).
+
+## Installation instructions
+
+[https://itunes.apple.com/us/app/xcode/id497799835?ls=1&mt=12](Download Xcode from the Mac App Store, requires OS X 10.9.4+).
+
+Copy the repository
+<code>git clone git@github.com:calexity/tabulate.git
+</code>
+
+Open the Tabulate.xcodeproj in Xcode
+
+Press the play button to run
+
+
+## How many hours did it take to complete?
+About 5, I think 3 of those were spent trying to get the keyboard to move the content up.
+
+## Which required and optional stories have you completed?
+*Required*
+- Calculator is functional
+- Added 18%, 20% and 22% as options
+- When tapping into bill amount field, keyboard appears
+- When tapping outside keyboard, keyboard dismisses
+*Optional*
+- All fields move up when bill amount is tap so the field stays in view
+- Status bar is removed
+- Added app icons
+- Added launch screen
+- Added image
+- Changed colors and fonts
+
+
+## GIF walkthrough
+img style="max-width:100%;"  src="  "></img>
---
+++ Tabulate/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  Tip it
+//
+//  Created by Alexa Roman on 1/2/15.
+//  Copyright (c) 2015 Alexa Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var bottomSpace: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+
+        var billFieldString:NSMutableAttributedString = NSMutableAttributedString(string: "Bill Amount")
+        billFieldString.addAttribute(NSForegroundColorAttributeName, value: UIColor.whiteColor(), range: NSRange(location:0,length:billFieldString.length))
+        billField.attributedPlaceholder = billFieldString
+ 
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+            
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        var info = notification.userInfo!
+        var keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.bottomSpace.constant = keyboardFrame.size.height + 10
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.bottomSpace.constant = 0
+        })
+    }
+    
+}
+
